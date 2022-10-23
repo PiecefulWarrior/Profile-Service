4decf1bd855de7dae058d2dbea30897b3061d3c4
bhushan <shimpibhushan6@gmail.com>
2022-10-23 22:34:11 +0530

    exception messeges moved to properties file

--- src/main/java/com/hrms/EmployeeProfile/EmployeeProfile.java
+++ src/main/java/com/hrms/EmployeeProfile/EmployeeProfile.java
@@ -1,12 +1,17 @@
 package com.hrms.EmployeeProfile;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:messeges.properties")
 public class EmployeeProfile{
-
+	
+	
 	public static void main(String[] args){
+	
 		SpringApplication.run(EmployeeProfile.class, args);
 	}
 
